30fe68e47320ee1f8f4f9bfd86d498429b003ec3
Victor Henrique Silva <henrictor@hotmail.com>
2021-09-15 19:18:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp_Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the first number: ");
+            int first_number = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int second_number = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the operation: ");
+            string operation = Console.ReadLine();
+            if (operation == "+"){
+                Console.WriteLine(first_number + second_number);
+            }
+            else if (operation == "-"){
+                Console.WriteLine(first_number - second_number);
+            }
+            else if (operation == "/"){
+                Console.WriteLine(first_number / second_number);
+            }
+            else if (operation == "*" || operation == "x"){
+                Console.WriteLine(first_number * second_number);
+            }
+            else{
+                Console.WriteLine("Operation or Number invalid, trynna again.");
+            }
+        }
+    }
+}
